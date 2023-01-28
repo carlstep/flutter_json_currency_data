3ae9b8df7826d72d0918fa082477a5babc256e04
carlstep <carlstep333@me.com>
2023-01-28 13:29:45 +0700

    final code - not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,65 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Currency>> fetchCurrencies(http.Client client) async {
+  final response =
+      await client.get(Uri.parse('https://floatrates.com/daily/usd.json'));
+
+  return parseCurrencies(response.body);
+}
+
+// a function that converts a response body into a List<Currency>
+List<Currency> parseCurrencies(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Currency>((json) => Currency.fromJson(json)).toList();
+}
+
+class Currency {
+  final String code;
+  final String alphaCode;
+  final String numericCode;
+  final String name;
+  final double rate;
+  final String? date;
+  final double inverseRate;
+
+  Currency({
+    required this.code,
+    required this.alphaCode,
+    required this.numericCode,
+    required this.name,
+    required this.rate,
+    required this.date,
+    required this.inverseRate,
+  });
+
+  factory Currency.fromJson(Map<String, dynamic> json) {
+    return Currency(
+      code: json["code"] as String,
+      alphaCode: json["alphaCode"] as String,
+      numericCode: json["numericCode"] as String,
+      name: json["name"] as String,
+      rate: json["rate"].toDouble() as double,
+      date: json["date"] as String?,
+      inverseRate: json["inverseRate"].toDouble() as double,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "code": code,
+        "alphaCode": alphaCode,
+        "numericCode": numericCode,
+        "name": name,
+        "rate": rate,
+        "date": date,
+        "inverseRate": inverseRate,
+      };
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +68,67 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    String appTitle = 'JSON Currency Feed';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PageOne(title: appTitle),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class PageOne extends StatelessWidget {
+  const PageOne({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: FutureBuilder<List<Currency>>(
+          future: fetchCurrencies(http.Client()),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('...has error'),
+              );
+            } else if (snapshot.hasData) {
+              return CurrencyList(currencies: snapshot.data!);
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CurrencyList extends StatelessWidget {
+  const CurrencyList({
+    Key? key,
+    required this.currencies,
+  }) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<Currency> currencies;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ListView.builder(
+      itemCount: currencies.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Text(currencies[index].alphaCode),
+          title: Text(currencies[index].name),
+        );
+      },
     );
   }
 }
